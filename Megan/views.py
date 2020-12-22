a982e424af17acffe2a73daabd767094dcf009f5
Alvin Tan <alvin.tan1912@gmail.com>
2020-12-22 15:59:42 +0800

    Working GET and POST requests

--- Megan/views.py
+++ Megan/views.py
@@ -1,8 +1,9 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import UserReplies
 from .serializers import UserRepliesSerializer
 from .apps import MeganConfig
@@ -10,17 +11,12 @@ from .apps import MeganConfig
 # Create your views here.
 PREDICTION_ENDPOINT = "https://megan-test-46.herokuapp.com/v1/models/LSTM:predict"
 
-@csrf_exempt
-def call(request):
-    """
-    Function info
-    """
-    if request == 'GET':
-        data = UserReplies('Hello! I am Megan!')
-        text = UserRepliesSerializer(data, many=True)
-        return Response(text.data)
+class call_model(APIView):
 
-    elif request == 'POST':
-        data = MeganConfig.parse(request)
-        prediction = requests.post(PREDICTION_ENDPOINT, json=data)
-        return prediction.text
+    def get(self, request, *args, **kwargs):
+        return JsonResponse("Hello! I'm Megan!", safe=False)
+    
+    def post(self, request, *args, **kwargs):
+        parsed = MeganConfig.parse(str(request.data))
+        prediction = requests.post(PREDICTION_ENDPOINT, json=parsed)
+        return JsonResponse(prediction.text, safe=False)
