4e4d41bf7f34e89878eafe93fd0f79556d6d3bad
Alvin Tan <alvin.tan1912@gmail.com>
2020-12-29 15:14:18 +0800

    Managing staticfiles

--- MegBackend46/settings.py
+++ MegBackend46/settings.py
@@ -129,8 +129,5 @@ AWS_LOCATION = 'static'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'MegBackend46/static')
-]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
