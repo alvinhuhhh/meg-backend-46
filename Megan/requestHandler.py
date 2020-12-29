a4362caa030d79de0521d64f41c3efac6ea8f759
Alvin Tan <alvin.tan1912@gmail.com>
2020-12-29 16:32:24 +0800

    Implemented requestHandler

---
+++ Megan/requestHandler.py
@@ -0,0 +1,57 @@
+import json
+import requests
+from .preprocessing import PreProcess
+from .preprocessing import ProcessResult
+
+PREDICTION_ENDPOINT = "https://megan-test-46.herokuapp.com/v1/models/LSTM:predict"
+
+class Handler:
+
+    def one(self, body=None):
+        return "Hey! I'm Megan!"
+
+    def two(self, body=None):
+        return "What's your name?"
+
+    def three(self, body=None):
+        return "Hi User! So how you feeling today?"
+
+    def four(self, body=None):
+        parsed = PreProcess(body["text"])
+        predictions = requests.post(PREDICTION_ENDPOINT, json=parsed)
+        result = ProcessResult(predictions.text)
+        if result == 0:
+            return "Aiyo why liddat? Its okay sometimes I feel liddat also..."
+        elif result == 4:
+            return "Swee! I see you happy I also shiok!"
+        else:
+            return "Error!"
+
+    stages = {"1": one, "2": two, "3": three, "4": four}
+
+    def handle(self, body):
+        """
+        Input:
+        body(dict)
+        Output:
+        response(string)
+        """
+        response = self.stages.get(body["stage"])
+        return response(self, body)
+        
+#########
+# Debug #
+#########
+def main():
+    test = {
+        "id": "1",
+        "stage": "4",
+        "type": "OE Response",
+        "text": "i am sad @alvinhuhhh"
+    }
+    testHandler = Handler()
+    print(testHandler.handle(test))
+    return
+
+if __name__ == "__main__":
+    main()
--- Megan/views.py
+++ Megan/views.py
@@ -1,10 +1,10 @@
-import requests
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from .apps import MeganConfig
+from .requestHandler import Handler
 
 # Create your views here.
 PREDICTION_ENDPOINT = "https://megan-test-46.herokuapp.com/v1/models/LSTM:predict"
@@ -15,7 +15,9 @@ class call_model(APIView):
         return JsonResponse("Hello! I'm Megan!", safe=False)
     
     def post(self, request, *args, **kwargs):
-        parsed = MeganConfig.parse(str(request.data['text']))
-        prediction = requests.post(PREDICTION_ENDPOINT, json=parsed)
-        result = MeganConfig.decode(prediction.text)
-        return JsonResponse(str(result), safe=False)
+        requestHandler = Handler()
+        response = requestHandler.handle(request.data)
+        # parsed = MeganConfig.parse(str(request.data['text']))
+        # prediction = requests.post(PREDICTION_ENDPOINT, json=parsed)
+        # result = MeganConfig.decode(prediction.text)
+        return JsonResponse(response, safe=False)
