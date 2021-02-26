0d19912c776c90885cf73ca050d9f4af564b5333
Alvin Tan <alvin.tan1912@gmail.com>
2021-02-26 15:16:56 +0800

    Registered UserMessages in admin site

--- Megan/admin.py
+++ Megan/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Replies
+from .models import UserMessages
 
 # Register your models here.
 admin.site.register(Replies)
+admin.site.register(UserMessages)
