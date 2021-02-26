a14c7cbfc431992314c9e042e14bd914abaea73f
Alvin Tan <alvin.tan1912@gmail.com>
2021-02-26 14:32:41 +0800

    New field in database: Users

--- Megan/models.py
+++ Megan/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Replies(models.Model):
     stage = models.IntegerField()
     response_type = models.CharField(max_length=50)
     text = models.TextField()
     sentiment = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.text
+
+
+class Users(models.Model):
+    user_id = models.IntegerField()
+    stage = models.IntegerField()
+    messages = models.JSONField()
 
     def __str__(self):
         return self.text
